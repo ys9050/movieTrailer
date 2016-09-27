b973889b82dd11167b9919f936b0d89740d0fc5c
Yoon <ys9050@gmail.com>
2016-09-26 23:53:14 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import fresh_tomatoes
+import data
+
+movies=[data.v_for_vendetta, data.devil_wears_prada, data.batman_begins, data.oldboy, data.finding_nemo, data.orphan]
+fresh_tomatoes.open_movies_page(movies)
