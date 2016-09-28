773283650cff921a3ce45efd06ca0a8df63b1a53
Yoon <ys9050@gmail.com>
2016-09-27 23:41:20 -0700

    Addtion of comment and organization

--- data.py
+++ data.py
@@ -1,27 +1,50 @@
 import media
 
-v_for_vendetta = media.Movie("V for Vendetta",
-                        "Evey Hammond and her unlikely but instrumental part in bringing down the fascist government that has taken control of a futuristic Great Britain.",
-                        "https://images-na.ssl-images-amazon.com/images/M/MV5BOTI5ODc3NzExNV5BMl5BanBnXkFtZTcwNzYxNzQzMw@@._V1_UX182_CR0,0,182,268_AL_.jpg",
-                        "https://www.youtube.com/watch?v=k_13fFIrhPk")
-devil_wears_prada = media.Movie("The Devil Wears Prada",
-                        "Simple and naive just-graduated in journalism Andrea Sachs is hired to work as the second assistant of the powerful and sophisticated Miranda Priestly, the ruthless and merciless executive of the Runway fashion magazine.",
-                        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMyNjk4Njc3NV5BMl5BanBnXkFtZTcwNDkyMTEzMw@@._V1_UX182_CR0,0,182,268_AL_.jpg",
-                        "https://www.youtube.com/watch?v=XTDSwAxlNhc")
-batman_begins = media.Movie("Batman Begins",
-                        "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
-                        "https://images-na.ssl-images-amazon.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_UX182_CR0,0,182,268_AL_.jpg",
-                        "https://www.youtube.com/watch?v=vak9ZLfhGnQ")
-oldboy = media.Movie("Oldboy",
-                        "After being kidnapped and imprisoned for 15 years, Oh Dae-Su is released, only to find that he must find his captor in 5 days.",
-                        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTI3NTQyMzU5M15BMl5BanBnXkFtZTcwMTM2MjgyMQ@@._V1_UX182_CR0,0,182,268_AL_.jpg",
-                        "https://www.youtube.com/watch?v=2HkjrJ6IK5E")
-finding_nemo = media.Movie("Finding Nemo",
-                        "After his son is captured in the Great Barrier Reef and taken to Sydney, a timid clownfish sets out on a journey to bring him home.",
-                        "https://images-na.ssl-images-amazon.com/images/M/MV5BZTAzNWZlNmUtZDEzYi00ZjA5LWIwYjEtZGM1NWE1MjE4YWRhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
-                        "https://www.youtube.com/watch?v=wZdpNglLbt8")
-orphan = media.Movie("Orphan",
-                        "A husband and wife who recently lost their baby adopt a nine year-old girl who is not nearly as innocent as she claims to be.",
-                        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTU0NjQzNjg2Nl5BMl5BanBnXkFtZTcwNzc3NzA0Mg@@._V1_UX182_CR0,0,182,268_AL_.jpg",
-                        "https://www.youtube.com/watch?v=WgxVIB2WuHU")
+
+# Provides instances of Movie class for main function
+v_for_vendetta = media.Movie(
+        "V for Vendetta",
+        "Evey Hammond and her unlikely but instrumental part "
+        "in bringing down the fascist government that has taken "
+        "control of a futuristic Great Britain.",
+        "https://images-na.ssl-images-amazon.com/images/M/MV5BOTI5ODc3NzExNV5BMl5BanBnXkFtZTcwNzYxNzQzMw@@._V1_UX182_CR0,0,182,268_AL_.jpg", # noqa
+        "https://www.youtube.com/watch?v=k_13fFIrhPk")
+devil_wears_prada = media.Movie(
+        "The Devil Wears Prada",
+        "Simple and naive just-graduated in journalism Andrea Sachs "
+        "is hired to work as the second assistant of the powerful"
+        "and sophisticated Miranda Priestly, the ruthless "
+        "and merciless executive of the Runway fashion magazine.",
+        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMyNjk4Njc3NV5BMl5BanBnXkFtZTcwNDkyMTEzMw@@._V1_UX182_CR0,0,182,268_AL_.jpg", # noqa
+        "https://www.youtube.com/watch?v=XTDSwAxlNhc")
+batman_begins = media.Movie(
+        "Batman Begins",
+        "After training with his mentor, Batman begins "
+        "his fight to free crime-ridden Gotham City from"
+        "the corruption that Scarecrow and the League of Shadows "
+        "have cast upon it.",
+        "https://images-na.ssl-images-amazon.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_UX182_CR0,0,182,268_AL_.jpg", # noqa
+        "https://www.youtube.com/watch?v=vak9ZLfhGnQ")
+oldboy = media.Movie(
+        "Oldboy",
+        "After being kidnapped and imprisoned for 15 years, "
+        "Oh Dae-Su is released, only to find that he"
+        "must find his captor in 5 days.",
+        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTI3NTQyMzU5M15BMl5BanBnXkFtZTcwMTM2MjgyMQ@@._V1_UX182_CR0,0,182,268_AL_.jpg", # noqa
+        "https://www.youtube.com/watch?v=2HkjrJ6IK5E")
+finding_nemo = media.Movie(
+        "Finding Nemo",
+        "After his son is captured in the Great Barrier Reef "
+        "and taken to Sydney, a timid clownfish sets"
+        "out on a journey to bring him home.",
+        "https://images-na.ssl-images-amazon.com/images/M/MV5BZTAzNWZlNmUtZDEzYi00ZjA5LWIwYjEtZGM1NWE1MjE4YWRhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg", # noqa
+        "https://www.youtube.com/watch?v=wZdpNglLbt8")
+orphan = media.Movie(
+        "Orphan",
+        "A husband and wife who recently lost their baby adopt "
+        "a nine year-old girl who is not nearly as innocent"
+        "as she claims to be.",
+        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTU0NjQzNjg2Nl5BMl5BanBnXkFtZTcwNzc3NzA0Mg@@._V1_UX182_CR0,0,182,268_AL_.jpg", # noqa
+        "https://www.youtube.com/watch?v=WgxVIB2WuHU")
+
 
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import fresh_tomatoes
 import data
 
-movies=[data.v_for_vendetta, data.devil_wears_prada, data.batman_begins, data.oldboy, data.finding_nemo, data.orphan]
+# List of Movie instances needed for argument
+movies=[data.v_for_vendetta, data.devil_wears_prada,
+        data.batman_begins, data.oldboy,
+        data.finding_nemo, data.orphan]
+
+# open_movies_page method opens html using movies as argument
 fresh_tomatoes.open_movies_page(movies)
